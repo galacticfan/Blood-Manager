6b25c1c6daa2cbc7064deb43289880d4b773c660
galacticfan <galacticfan@server.fake>
2013-12-28 12:33:40 +0000

    Load file functionality

--- Blood Manager/Pages/LocalModeMain.xaml.cs
+++ Blood Manager/Pages/LocalModeMain.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Blood_Manager.Pages
 {
@@ -35,6 +36,42 @@ namespace Blood_Manager.Pages
         {
             Window window = Window.GetWindow(this);
             window.WindowState = WindowState.Minimized;
+        }
+        
+        // GLOBALS
+        string fileToLoad;
+        List<Person> people = new List<Person>();
+
+        // MAIN EVENTS
+        private void loadFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFD = new Microsoft.Win32.OpenFileDialog();
+            openFD.DefaultExt = ".xml";
+            openFD.Filter = "XML Document (.xml)|*.xml";
+
+            Nullable<bool> result = openFD.ShowDialog(); 
+            if (result == true) // check a file was selected
+            {
+                fileToLoad = openFD.FileName;
+            }
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(fileToLoad);
+            foreach (XmlNode xNode in xDoc.SelectNodes("People/Person"))
+            {
+                Person p = new Person();
+                p.Surename = xNode.SelectSingleNode("Surname").InnerText;
+                p.Forename = xNode.SelectSingleNode("Forename").InnerText;
+                p.BloodGroup = xNode.SelectSingleNode("BloodGroup").InnerText;
+                p.RhD = xNode.SelectSingleNode("RhD").InnerText;
+                p.Address = xNode.SelectSingleNode("Address").InnerText;
+                p.Phone = xNode.SelectSingleNode("Phone").InnerText;
+                p.Mobile = xNode.SelectSingleNode("Mobile").InnerText;
+                people.Add(p);
+
+                string fullName = p.Forename + " " + p.Surename;
+                listBoxNames.Items.Add(fullName);
+            }
         }
     }
 }
